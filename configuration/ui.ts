2bee19048bb1b7e7776d1f3332d40276ebaf8c47
Theo Kuelker <tkuelker@ad.unc.edu>
2025-02-19 16:42:52 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Finanical Valuation Specialist`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Type your message here...`;
 export const FOOTER_MESSAGE: string = `Customized AI by Theo Kuelker`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
+export const PAGE_TITLE: string = `Finanical Valuation Specialist`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
